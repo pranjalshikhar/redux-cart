cf85be72e5ec5fc31400fdc6ab3fdbc72d84b829
Pranjal Shikhar Sinha <shikhar.pranjal3@gmail.com>
2022-12-06 02:14:35 +0530

    commit

---
+++ src/components/Products.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { add } from "../store/cartSlice";
+
+const Products = () => {
+  const dispatch = useDispatch();
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const res = await fetch("https://fakestoreapi.com/products");
+      const data = await res.json();
+      console.table(data);
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+
+  const handleAdd = (product) => {
+    dispatch(add(product));
+  };
+
+  return (
+    <div className="productsWrapper">
+      {products.map((product) => (
+        <div className="card" key={product.id}>
+          <img src={product.image} alt={product.title} />
+          <h4>{product.title}</h4>
+          <h5>{product.price}</h5>
+          <button onClick={() => handleAdd(product)} className="btn">
+            Add to cart
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Products;
---
+++ src/pages/Cart.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { remove } from "../store/cartSlice";
+
+const Cart = () => {
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.cart);
+
+  const handleRemove = (product) => {
+    dispatch(remove(product));
+  };
+
+  return (
+    <div>
+      <h3>Cart</h3>
+      <div className="cartWrapper">
+        {products.map((product) => (
+          <div className="cartCard" key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <h4>{product.title}</h4>
+            <h5>{product.price}</h5>
+            <button onClick={() => handleRemove(product.id)} className="btn">
+              Remove
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
